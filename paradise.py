3751c4b971cdcad3e8fbb6457d4d425f7ecac5d4
JR Ladd <johnrobertladd@gmail.com>
2018-11-17 13:37:58 -0600

    updated code, test files, and svg

--- paradise.py
+++ paradise.py
@@ -2,11 +2,14 @@
 
 import spacy
 from spacy import displacy
-import requests
-from random import sample
+import requests, re
+from pathlib import Path
+# from random import sample
 
+def string_insert(source_str, insert_str, pos):
+    return source_str[:pos]+insert_str+source_str[pos:]
+    
 nlp = spacy.load('en')
-#text = u"""In ancient Rome, some neighbors live in three adjacent houses. In the center is the house of Senex, who lives there with wife Domina, son Hero, and several slaves, including head slave Hysterium and the musical's main character Pseudolus. A slave belonging to Hero, Pseudolus wishes to buy, win, or steal his freedom. One of the neighboring houses is owned by Marcus Lycus, who is a buyer and seller of beautiful women; the other belongs to the ancient Erronius, who is abroad searching for his long-lost children (stolen in infancy by pirates). One day, Senex and Domina go on a trip and leave Pseudolus in charge of Hero. Hero confides in Pseudolus that he is in love with the lovely Philia, one of the courtesans in the House of Lycus (albeit still a virgin)."""
 
 with open('paradiselost.txt', 'r') as pl:
     text = pl.read()
@@ -15,20 +18,39 @@ doc = nlp(text[:10000])
 sentence_spans = list(doc.sents)
 options= {'compact': True, 'collapse_punct': True }#, 'distance': 50 }
 #displacy.serve(sentence_spans, style='dep', options=options)
-for s in sentence_spans:
-    #html = displacy.render(s, style='dep', options=options)
-    print(s)
+full_md = ""
+for s_idx, s in enumerate(sentence_spans):
+    # svg = displacy.render(s, style='dep', options=options)
+    filename = "img/sentence{}.svg".format(s_idx)
+    # output_path = Path(filename)
+    # output_path.open('w', encoding='utf-8').write(svg)
+    full_sentence = s.text
+    offset = 0
     for token in s:
         if token.pos_ == "NOUN":#or token.pos_ == "PROPN":
-            print(token.text)
+            # print(token.text)
+            idx = token.idx - s.start_char
+            print(idx)
+            note_idx = idx + len(token.text) + offset
             url = 'http://api.conceptnet.io/c/en/{}'.format(token.text.lower().strip('.,:;"-!?'))
-            print(url)
+            # print(url)
             obj = requests.get(url).json()
-            if len(obj['edges']) > 2:
-                edges = sample(obj['edges'], 2)
-                for e in edges:
-                    print(e['surfaceText'])
-    print()
+            note = []
+            if len(obj['edges']) > 0:
+                # edges = sample(obj['edges'], 2)
+                for e in obj['edges']:
+                    if e['surfaceText']:
+                        sentence = re.sub(r"\[|\]|\*", "", e['surfaceText'])
+                        sentence = "{}.".format(sentence.capitalize())
+                        note.append(sentence)
+            note = "^[{}]".format(" ".join(note))
+            full_sentence = string_insert(full_sentence, note, note_idx)
+            offset += len(note)
+    full_sentence = full_sentence.replace("\n", "\n\n")
+    full_md += "{}\n\n![]({})\n\n".format(full_sentence, filename)
+    # print()
     #print(html)
 # with open('dependency.html', 'w') as newfile:
 #     newfile.write(html)
+with open('test.md', 'w') as newfile:
+    newfile.write(full_md)
