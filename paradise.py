d670aa9e587d4e4002c78f67ecfe1f20831fb40d
JR Ladd <johnrobertladd@gmail.com>
2018-11-17 18:35:25 -0600

    clean up code, deal with unicode

--- paradise.py
+++ paradise.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python
 
 import spacy
 from spacy import displacy
@@ -8,49 +8,57 @@ from pathlib import Path
 
 def string_insert(source_str, insert_str, pos):
     return source_str[:pos]+insert_str+source_str[pos:]
-    
-nlp = spacy.load('en')
 
-with open('paradiselost.txt', 'r') as pl:
-    text = pl.read()
-nlp.add_pipe(nlp.create_pipe('sentencizer'), before='parser')
-doc = nlp(text[:10000])
-sentence_spans = list(doc.sents)
-options= {'compact': True, 'collapse_punct': True }#, 'distance': 50 }
-#displacy.serve(sentence_spans, style='dep', options=options)
-full_md = ""
-for s_idx, s in enumerate(sentence_spans):
-    # svg = displacy.render(s, style='dep', options=options)
-    filename = "img/sentence{}.svg".format(s_idx)
-    # output_path = Path(filename)
-    # output_path.open('w', encoding='utf-8').write(svg)
-    full_sentence = s.text
+def create_svg(sentence, filename):
+    svg = displacy.render(sentence, style='dep', options=options)
+    output_path = Path(filename)
+    output_path.open('w', encoding='utf-8').write(svg)
+
+def create_annotated_text(sentence, sentence_str):
     offset = 0
-    for token in s:
-        if token.pos_ == "NOUN":#or token.pos_ == "PROPN":
-            # print(token.text)
-            idx = token.idx - s.start_char
-            print(idx)
+    for token in sentence:
+        if token.pos_ == "NOUN":
+            idx = token.idx - sentence.start_char
             note_idx = idx + len(token.text) + offset
             url = 'http://api.conceptnet.io/c/en/{}'.format(token.text.lower().strip('.,:;"-!?'))
-            # print(url)
             obj = requests.get(url).json()
-            note = []
-            if len(obj['edges']) > 0:
-                # edges = sample(obj['edges'], 2)
-                for e in obj['edges']:
-                    if e['surfaceText']:
-                        sentence = re.sub(r"\[|\]|\*", "", e['surfaceText'])
-                        sentence = "{}.".format(sentence.capitalize())
-                        note.append(sentence)
-            note = "^[{}]".format(" ".join(note))
-            full_sentence = string_insert(full_sentence, note, note_idx)
+            note = create_note(obj)
+            sentence_str = string_insert(sentence_str, note, note_idx)
             offset += len(note)
-    full_sentence = full_sentence.replace("\n", "\n\n")
-    full_md += "{}\n\n![]({})\n\n".format(full_sentence, filename)
-    # print()
-    #print(html)
-# with open('dependency.html', 'w') as newfile:
-#     newfile.write(html)
-with open('test.md', 'w') as newfile:
-    newfile.write(full_md)
+    sentence_str = sentence_str.replace("\n", "\n\n")
+    return sentence_str
+
+def create_note(obj):
+    note = []
+    if len(obj['edges']) > 0:
+        for e in obj['edges']:
+            if e['surfaceText'] and "translation" not in e['surfaceText']:
+                print(e['rel']['label'])
+                note_sentence = re.sub(r"\[|\]|\*", "", e['surfaceText'])
+                note_sentence = "{}.".format(note_sentence.capitalize())
+                note.append(note_sentence)
+    note = "^[{}]".format(" ".join(note))
+    return note
+
+if __name__ == '__main__':
+
+    nlp = spacy.load('en')
+
+    with open('paradiselost.txt', 'r') as pl:
+        text = pl.read()
+    nlp.add_pipe(nlp.create_pipe('sentencizer'), before='parser')
+    doc = nlp(text[:10000])
+    sentence_spans = list(doc.sents)
+    options= {'compact': True, 'collapse_punct': True }#, 'distance': 50 }
+    #displacy.serve(sentence_spans, style='dep', options=options)
+    full_md = ""
+    for s_idx, sentence in enumerate(sentence_spans):
+
+        filename = "img/sentence{}.svg".format(s_idx)
+        # create_svg(sentence, filename)
+        sentence_str = sentence.text
+        sentence_str = create_annotated_text(sentence, sentence_str)
+        full_md += "{}\n\n![]({})\n\n".format(sentence_str, filename)
+
+    with open('test.md', 'w') as newfile:
+        newfile.write(full_md)
